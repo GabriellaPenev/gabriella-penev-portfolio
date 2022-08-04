faafcd04f4b8ab7c82fb6bcc7722603e5eb8186f
GabriellaPenev <gabriellapenev@gmail.com>
2022-08-04 18:08:54 -0400

    working on media queries

---
+++ scripts.js
@@ -0,0 +1,6 @@
+
+
+// mobile nav
+// if desktopNav is display: hidden then run some code
+// function to add a .mobileNav nav to the header as the last child, with the hamburger 3 lines in a span
+// on click of the 3 span lines, the 
