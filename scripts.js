94eb8e52a2b33d22fe97c4c6352e9c65a2e9fb56
GabriellaPenev <gabriellapenev@gmail.com>
2022-09-28 19:01:50 -0400

    Added screenwriter portfolio to projects section

--- scripts.js
+++ scripts.js
@@ -1,15 +1,40 @@
-const portfolioApp = {}
+const portfolio = {}
 
-portfolioApp.init = () => {
+portfolio.descSpan = document.getElementById('descriptions');
+
+portfolio.descriptions = [
+    'live',
+    'develop',
+    'code',
+    'create'
+]
+
+let count = 0;
+
+portfolio.cycleDescriptions = () => {
+    let index = count % portfolio.descriptions.length;
+    portfolio.descSpan.textContent = portfolio.descriptions[index];
+    count++;
+}
+
+portfolio.firstDescription = () => {
+    portfolio.descSpan.textContent = 'create';
+}
+
+
+portfolio.init = () => {
     window.addEventListener('scroll', function () {
         const header = document.querySelector('.fixedHeader');
     
-        if (window.pageYOffset > 20) {
+        if (window.pageYOffset > 5) {
             header.classList.add('shrink')
         } else {
             header.classList.remove('shrink');
         }
     })
+
+    portfolio.firstDescription();
+    setInterval(portfolio.cycleDescriptions, 3300);
 }
 
-portfolioApp.init();
+portfolio.init();
