c4178c9a6b2d4d5774ffc9bce062615c7caff8cf
GabriellaPenev <gabriellapenev@gmail.com>
2022-10-20 14:01:38 -0400

    added scroll to top functionality, revised media queries

--- scripts.js
+++ scripts.js
@@ -1,14 +1,25 @@
 const portfolio = {}
 
+let count = 0;
 portfolio.descSpan = document.getElementById('descriptions');
 
+portfolio.scrollTop = () => {
+    const button = document.querySelector('.btn');
+    
+    button.addEventListener( 'click', () => {
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        });
+    });
+}
+
 portfolio.descriptions = [
-    'live',
     'code',
+    'collaborate',
     'create'
 ]
-
-let count = 0;
 
 portfolio.cycleDescriptions = () => {
     let index = count % portfolio.descriptions.length;
@@ -20,20 +31,23 @@ portfolio.firstDescription = () => {
     portfolio.descSpan.textContent = 'create';
 }
 
-
-portfolio.init = () => {
-    window.addEventListener('scroll', function () {
+portfolio.shrinkHeader = () => {
+    window.addEventListener('scroll', () => {
         const header = document.querySelector('.fixedHeader');
     
         if (window.pageYOffset > 5) {
-            header.classList.add('shrink')
+            header.classList.add('shrink');
         } else {
             header.classList.remove('shrink');
         }
     })
+}
 
+portfolio.init = () => {
+    portfolio.shrinkHeader();
+    portfolio.scrollTop();
     portfolio.firstDescription();
-    setInterval(portfolio.cycleDescriptions, 2800);
+    setInterval(portfolio.cycleDescriptions, 2500);
 }
 
 portfolio.init();
